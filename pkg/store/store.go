ab008591773f7ad96184320dd38c83edede0e7a8
andriidski <24281657+andriidski@users.noreply.github.com>
2023-01-20 23:04:16 -0800

    feat: postgres store (#2)
    * feat: postgres store
    
    * add validator stats endpoints for db storer

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -2,77 +2,24 @@ package store
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/ralexstokes/relay-monitor/pkg/types"
 )
 
+// Interface for the state "storer" that a relay monitor can use for storing data.
 type Storer interface {
 	PutBid(context.Context, *types.BidContext, *types.Bid) error
 	PutValidatorRegistration(context.Context, *types.SignedValidatorRegistration) error
 	PutAcceptance(context.Context, *types.BidContext, *types.SignedBlindedBeaconBlock) error
 
 	GetBid(context.Context, *types.BidContext) (*types.Bid, error)
+
 	// `GetValidatorRegistrations` returns all known registrations for the validator's public key, sorted by timestamp (increasing).
-	GetValidatorRegistrations(context.Context, *types.PublicKey) ([]types.SignedValidatorRegistration, error)
+	GetValidatorRegistrations(context.Context, *types.PublicKey) ([]*types.SignedValidatorRegistration, error)
+	// `GetLatestValidatorRegistration` returns the latest known registration for the validator's public key.
+	GetLatestValidatorRegistration(context.Context, *types.PublicKey) (*types.SignedValidatorRegistration, error)
 	// `GetCountValidatorsRegistrations`returns the total number of valid registrations processed.
 	GetCountValidatorsRegistrations(ctx context.Context) (uint, error)
 	// `GetCountValidators`returns the number of validators that have successfully submitted at least one registration.
 	GetCountValidators(ctx context.Context) (uint, error)
-}
-
-type MemoryStore struct {
-	bids          map[types.BidContext]*types.Bid
-	registrations map[types.PublicKey][]types.SignedValidatorRegistration
-	acceptances   map[types.BidContext]types.SignedBlindedBeaconBlock
-}
-
-func NewMemoryStore() *MemoryStore {
-	return &MemoryStore{
-		bids:          make(map[types.BidContext]*types.Bid),
-		registrations: make(map[types.PublicKey][]types.SignedValidatorRegistration),
-		acceptances:   make(map[types.BidContext]types.SignedBlindedBeaconBlock),
-	}
-}
-
-func (s *MemoryStore) PutBid(ctx context.Context, bidCtx *types.BidContext, bid *types.Bid) error {
-	s.bids[*bidCtx] = bid
-	return nil
-}
-
-func (s *MemoryStore) GetBid(ctx context.Context, bidCtx *types.BidContext) (*types.Bid, error) {
-	bid, ok := s.bids[*bidCtx]
-	if !ok {
-		return nil, fmt.Errorf("could not find bid for %+v", bidCtx)
-	}
-	return bid, nil
-}
-
-func (s *MemoryStore) PutValidatorRegistration(ctx context.Context, registration *types.SignedValidatorRegistration) error {
-	publicKey := registration.Message.Pubkey
-	registrations := s.registrations[publicKey]
-	registrations = append(registrations, *registration)
-	s.registrations[publicKey] = registrations
-	return nil
-}
-
-func (s *MemoryStore) PutAcceptance(ctx context.Context, bidCtx *types.BidContext, acceptance *types.SignedBlindedBeaconBlock) error {
-	s.acceptances[*bidCtx] = *acceptance
-	return nil
-}
-
-func (s *MemoryStore) GetValidatorRegistrations(ctx context.Context, publicKey *types.PublicKey) ([]types.SignedValidatorRegistration, error) {
-	return s.registrations[*publicKey], nil
-}
-
-func (s *MemoryStore) GetCountValidatorsRegistrations(ctx context.Context) (uint, error) {
-	var totalRegistrations uint
-	for _, signedRegistrations := range s.registrations {
-		totalRegistrations += uint(len(signedRegistrations))
-	}
-	return totalRegistrations, nil
-}
-
-func (s *MemoryStore) GetCountValidators(ctx context.Context) (uint, error) {
-	return uint(len(s.registrations)), nil
 }
